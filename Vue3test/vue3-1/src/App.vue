0898db8d2fd323855741af19a6592e60a8ecc711
sahdj <1605482778@qq.com>
2021-04-16 23:02:17 +0800

    Vue3

---
+++ Vue3test/vue3-1/src/App.vue
@@ -0,0 +1,67 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-04-16 16:02:15
+ * @LastEditTime: 2021-04-16 21:23:29
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \Vue3\vue3-1\src\App.vue
+-->
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <h2>欢迎光临红浪漫洗浴中心</h2>
+  <div>请选择一位美女为你服务</div>
+  <div>
+    <button 
+      v-for = "(item,index) in girls" 
+      v-bind:key = "index"
+      @click="selectGirlFun(index)"
+    >
+      {{ index }} : {{ item }}
+    </button>
+  </div>
+  <div>你选择了【{{selectGirl}}】为你服务</div>
+
+  <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, reactive } from 'vue';
+
+// import HelloWorld from './components/HelloWorld.vue';
+
+export default defineComponent({
+  name: 'App',
+
+  //setup()和ref()函数
+  //setup 函数的用法，可以代替 Vue2 中的 date 和 methods 属性，直接把逻辑写在 setup 里就可以
+  //return出去的数据和方法，在模板中才可以使用，这样可以精准的控制暴漏的变量和方法
+  setup() {
+    const girls = ref (['西施','貂蝉','王昭君','杨玉环']);
+    const selectGirl = ref("");
+    const selectGirlFun = (index: number) => {
+      selectGirl.value = girls.value[index];
+    };
+    //因为在模板中这些变量和方法都需要条用，所以需要return出去。
+    return {
+      girls,
+      selectGirl,
+      selectGirlFun,
+    };
+  },
+
+  // components: {
+  //   HelloWorld
+  // }
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Vue3test/vue3-2/src/App.vue
@@ -0,0 +1,67 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-04-16 21:39:28
+ * @LastEditTime: 2021-04-16 22:21:23
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \Vue3\Vue3test\vue3-2\src\App.vue
+-->
+<template>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <div>
+      <h2>欢迎光临红浪漫洗浴中心</h2>
+      <div>请选择一位美女为你服务</div>
+    </div>
+    <div>
+      <button
+        v-for="(item, index) in data.girls"
+        v-bind:key="index"
+        @click="data.selectGirlFun(index)"
+      >
+        {{ index }} : {{ item }}
+      </button>
+    </div>
+    <div>你选择了【{{ data.selectGirl }}】为你服务</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, reactive } from 'vue';
+
+//定义一个接口,用接口(interface)来作类型注解
+interface DataProps {
+  girls: string[];
+  selectGirl: string;
+  selectGirlFun: (index: number) => void;
+}
+
+//reactive也是一个函数(方法)，只不过里边接受的参数是一个 Object(对象)
+export default defineComponent ({
+  name: 'App',
+    setup() { //在显示的为 data 变量作一个类型注解 cosnt data: DataProps = ...
+    const data: DataProps = reactive ({
+      girls: ['西施','貂蝉','王昭君','杨玉环'],
+      selectGirl: "",
+      selectGirlFun: (index: number) => {
+        data.selectGirl = data.girls[index];
+      },
+    });
+
+    return {
+      data,
+    };
+  },
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>

f48a0dc409336454210a73a4ff59d3b5c1d4cfe2
sahdj <1605482778@qq.com>
2021-04-28 22:58:11 +0800

    Vue3

--- Vue3project/ewshop/src/views/Home.vue
+++ Vue3project/ewshop/src/views/Home.vue
@@ -1,31 +1,37 @@
 <!--
  * @Author: your name
  * @Date: 2021-04-26 16:34:51
- * @LastEditTime: 2021-04-27 22:14:13
+ * @LastEditTime: 2021-04-28 21:51:47
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \Vue3project\ewshop\src\views\Home.vue
 -->
 <template>
   <div class="home">
-    
+    {{ banner }}
   </div>
 </template>
 
 <script>
-import { onMounted } from 'vue';
+import { ref, onMounted } from 'vue';
 import { getHomeAllData } from '@/network/home';
 
 export default {
   name: 'Home',
   setup() {
+    const banner = ref([]);
+
     onMounted(() => {
       getHomeAllData().then(res => {
-        console.log(res.data.slides);
+        banner.value = res.slides;
       }).catch (err => {
         console.log(err);
       })
     })
+
+    return {
+      banner,
+    }
   },
   components: {
 

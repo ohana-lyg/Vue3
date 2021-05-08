75a0219177249151e6ff0a2f897143484e6cbcdc
sahdj <1605482778@qq.com>
2021-05-08 23:46:40 +0800

    Vue3

--- Vue3project/ewshop/src/main.js
+++ Vue3project/ewshop/src/main.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-04-26 16:34:51
- * @LastEditTime: 2021-05-07 22:27:10
+ * @LastEditTime: 2021-05-08 14:46:14
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \ewshop\src\main.js
@@ -12,10 +12,11 @@ import router from './router'
 import store from './store'
 import 'vant/lib/index.css'
 
-import { Swipe, SwipeItem, Lazyload, Badge, Sidebar, SidebarItem, Collapse, CollapseItem } from 'vant';
+import { Swipe, SwipeItem, Lazyload, Badge, Sidebar, SidebarItem, Collapse, CollapseItem, Tab, Tabs, Card } from 'vant';
 
 createApp(App)
     .use(Swipe).use(SwipeItem).use(Lazyload, {
         loading: require('./assets/images/3.png')
     }).use(Badge).use(Sidebar).use(SidebarItem).use(Collapse).use(CollapseItem)
+    .use(Tab).use(Tabs).use(Card)
     .use(store).use(router).mount('#app')
--- Vue3project/ewshop/src/views/category/Category.vue
+++ Vue3project/ewshop/src/views/category/Category.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2021-04-29 21:25:29
- * @LastEditTime: 2021-05-07 22:45:33
+ * @LastEditTime: 2021-05-08 23:42:38
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \Vue3project\ewshop\src\views\category\Category.vue
@@ -13,7 +13,13 @@
             <template v-slot:right></template>
         </nav-bar>
         <div id="mainbox">
-            <div class="ordertab"></div>
+            <div class="ordertab">
+                <van-tabs v-model="active" @click="tabClick">
+                    <van-tab title="销量排序"></van-tab>
+                    <van-tab title="价格排序"></van-tab>
+                    <van-tab title="评论排序"></van-tab>
+                </van-tabs>
+            </div>
             <van-sidebar class="leftmenu" v-model="activeKey">
                 <van-collapse v-model="activeName" accordion>
                     <van-collapse-item 
@@ -24,36 +30,111 @@
                         
                         <van-sidebar-item 
                             v-for="sub in item.children" 
-                            :key="sub" 
-                            :title="sub.name" />
+                            :key="sub.id" 
+                            :title="sub.name" 
+                            @click="getGoods(sub.id)"
+                        />
                     </van-collapse-item>
                 </van-collapse>
             </van-sidebar>
-            <div class="goodslist"></div>
+            <div class="goodslist">
+                <div class="content">
+                    <van-card
+                        v-for="item in ShowGoods"
+                        :key="item.id" 
+                        :num="item.sales"
+                        :tag="item.comments_count >= 0 ? '热门' : '一般'"
+                        :price="item.price"
+                        :desc="item.updated_at"
+                        :title="item.title"
+                        :thumb="item.cover_url"
+                    />
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
 import NavBar from "../../components/common/navbar/NavBar";
-import { getCategory } from "../../network/category";
-import { ref, onMounted } from 'vue';
+import { getCategory, getCategoryGoods } from "../../network/category";
+import { ref, onMounted, reactive, computed } from 'vue';
 export default {
     name: "Category",
     setup() {
+        let active = ref(1);
         let activeKey = ref(0);
         let activeName = ref(1);
         let categories = ref([]);
+        //当前排序
+        let currentOrders = ref('sales');
+        //当前的分类商品
+        let currrentCid = ref(0);
+
+        //数据模型
+        const goods = reactive({
+            sales:{ page: 1, list: []},
+            price:{ page: 1, list: []},
+            comments_count:{ page: 1, list: []}
+        })
+
+        const ShowGoods = computed(() => {
+            return goods[currentOrders.value].list;
+        })
+
+        const init = () => {
+            getCategoryGoods('sales', currrentCid.value).then( res => {
+                goods.sales.list = res.goods.data;
+            })
+            getCategoryGoods('price', currrentCid.value).then( res => {
+                goods.price.list = res.goods.data;
+            })
+            getCategoryGoods('comments_count', currrentCid.value).then( res => {
+                goods.comments_count.list = res.goods.data;
+            })
+        }
+
 
         onMounted(() => {
             getCategory().then(res => {
                 categories.value = res.categories;
             })
+            getCategoryGoods('sales', currrentCid.value).then( res => {
+                goods.sales.list = res.goods.data;
+            })
+            // init();
         })
+        //排序选项卡
+        const tabClick = (index) => {
+            let orders = ['sales','price','comments_count'];
+            currentOrders.value = orders[index];
+            getCategoryGoods(currentOrders.value, currrentCid.value).then( res => {
+                goods[currentOrders.value].list = res.goods.data;
+            })
+
+            // console.log("当前分类：" + currrentCid.value);
+            // console.log("当前排序：" + currentOrders.value);
+        }
+        //通过分类得到商品
+        const getGoods = (cid) => {
+            currrentCid.value = cid;
+            init();
+
+            // console.log("当前分类：" + currrentCid.value);
+            // console.log("当前排序：" + currentOrders.value);
+        }
+
         return {
+            active,
             activeKey,
             activeName,
-            categories
+            categories,
+            tabClick,
+            currentOrders,
+            getGoods,
+            goods,
+            currrentCid,
+            ShowGoods
         }
     },
     components: {
@@ -70,12 +151,12 @@ export default {
         flex: 1;
         float: right;
         position: fixed;
+        top: 45px;
         right: 0;
         left: 130px;
         // background: pink;
-        width: 100%;
         height: 50px;
-        // z-index: 6;
+        z-index: 6;
     }
 
     .leftmenu {
@@ -94,6 +175,11 @@ export default {
         left: 130px;
         right: 0;
         height: 100vh;
+        padding: 10px;
+        text-align: left !important;
     }
+}
+.van-card__thumb {
+    width: 68px !important;
 }
 </style>

4ba56c67f04ce1f8c9956645a7c052763a80e42b
sahdj <1605482778@qq.com>
2021-05-16 22:59:44 +0800

    Vue3

--- Vue3project/ewshop/src/views/profile/Register.vue
+++ Vue3project/ewshop/src/views/profile/Register.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2021-05-15 21:01:24
- * @LastEditTime: 2021-05-15 21:02:30
+ * @LastEditTime: 2021-05-16 22:56:15
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \ewshop\src\views\profile\Register.vue
@@ -13,6 +13,7 @@
 <script>
 export default {
     name: 'Register',
+    
 }
 </script>
 

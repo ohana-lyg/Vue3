6013fbc4e58dc1f2c18b0aed3c6b4ee1a6bed5c6
sahdj <1605482778@qq.com>
2021-05-22 22:36:47 +0800

    Vue3

--- Vue3project/ewshop/src/views/shopcart/ShopCart.vue
+++ Vue3project/ewshop/src/views/shopcart/ShopCart.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2021-04-29 21:30:16
- * @LastEditTime: 2021-04-30 21:06:01
+ * @LastEditTime: 2021-05-22 22:36:12
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \Vue3project\ewshop\src\views\shopcart\ShopCart.vue
@@ -10,7 +10,6 @@
     <div>
         <nav-bar>
             <template v-slot:default>购物车</template>
-            <template v-slot:right></template>
         </nav-bar>
     </div>
 </template>
